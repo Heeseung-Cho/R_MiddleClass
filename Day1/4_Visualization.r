eb431fd427d6618721ab3c3c910b93d278991d52
Cho Heeseung <68883298+Heeseung-Cho@users.noreply.github.com>
2021-03-18 19:52:55 +0900

    Update 4_Visualization.r

--- Day1/4_Visualization.r
+++ Day1/4_Visualization.r
@@ -40,3 +40,45 @@ pie(wid_mean$Sepal.Width,wid_mean$Species, radius = 2) #plot4
 par(mfrow = c(1,1))
 
 
+
+## ggplot2
+
+# Import library ggplot2
+library(ggplot2)
+
+base1 = ggplot(data = iris, mapping = aes(x = Sepal.Width, y = Sepal.Length))
+base1 + geom_point() + ggtitle("Scatter")            #산점도
+base1 + geom_line()  + ggtitle("Line")               #선 그래프
+base1 + geom_point() + geom_line() + ggtitle("Both") #동시에
+
+base1 + geom_point() + ggtitle("Scatter with hline") +
+   geom_hline(yintercept = mean(iris$Sepal.Length)) #수평선 추가
+
+base2 = ggplot(data = iris, mapping = aes(x = Sepal.Width, y = Species))
+base2 + geom_boxplot(notch = TRUE) + ggtitle("Box")  #박스 그래프
+base2 + geom_violin() + ggtitle("Violin")            #바이올린 그래프
+
+base3 = ggplot(data = iris, mapping = aes(x = Petal.Length))
+base3 + geom_histogram(binwidth = 1) +ggtitle("Histogram") #히스토그램
+
+## Stat
+
+## Scale
+install.packages("patchwork")
+library(patchwork)
+
+base1 + geom_point() + ggtitle("Remove Labels") +  #Label 제거
+  xlab("") + ylab("") + theme(text = element_text(size=20))
+base1 + geom_point() + ggtitle("Your Labels") +    #Labe 변경
+  xlab("Width") + ylab("Length")
+base1 + geom_point() + ggtitle("Limited") +        #범위 변경
+  xlim(c(2.5,4.0)) + ylim(c(5,7))
+base1 + geom_point(aes(alpha = Species)) + ggtitle("Alpha")+ #Species마다 진하기 변경
+base1 + geom_point(aes(alpha = Species)) + ggtitle("Alpha <0.3")+ #색상 진하기 변경
+  scale_alpha_discrete(range = c(0.1,0.3))
+base1 + geom_point(aes(colour = Species)) + ggtitle("Color")+
+  scale_colour_hue()
+
+## Coordinate
+base1 + geom_point() + coord_cartesian() + ggtitle("Cartesian") #직교좌표계
+base1 + geom_point() + coord_polar() + ggtitle("Polar")         #극좌표계
