ac740383a9d7fd93a7cb33ad89159731aad31e4a
Cho Heeseung <68883298+Heeseung-Cho@users.noreply.github.com>
2021-03-23 00:46:56 +0900

    Update 7_Regression.r

--- Day2/7_Regression.r
+++ Day2/7_Regression.r
@@ -1,15 +1,44 @@
-### Single-variate Linear Regression
-data(cars)
+## 0. Import Libraries
 library(ggplot2)
+library(MASS)
+
+## 1. Single-variate Linear Regression
+data(cars)
 ggplot(cars, aes(speed,dist))+ theme(text = element_text(size=20)) +
   geom_point()+
   geom_smooth(method="lm")
 
-#Linear regression code
-cars
-result = lm(dist ~ speed, data = cars)
-summary(result)
-
+data(cars)
+result1 = lm(dist ~ speed, data = cars)
+summary(result1)
 par(mfrow = c(2,2))
-plot(lm(dist ~ speed, data = cars))
+plot(result1)
 
+## 2. Multi-variate Linear Regression
+data(mtcars)
+result2 = lm(disp~cyl+hp+wt, data=mtcars)
+summary(result2)
+par(mfrow = c(2,2))
+plot(result2)
+
+# Forward selection
+step(lm(disp~1,data=mtcars),direction="forward", scope=(~cyl+hp+wt))
+# Backward selection
+step(lm(disp~cyl+hp+wt,data=mtcars),direction="backward")
+# Mixed selection
+step(lm(disp~1,data=mtcars),direction="both", scope=(~cyl+hp+wt))
+
+## 3. Logistic Regression
+data(biopsy)
+str(biopsy)
+prep_biopsy = na.omit(biopsy)[,-1] #Preprocessing
+
+result3 = glm(class ~ V1+V2+V4+V6+V7, data = prep_biopsy, family = 'binomial')
+summary(result3)
+
+### Confusion Matrix
+logit.probs = predict(result3,prep_biopsy, type = "response")
+logit.pred = ifelse(logit.probs > .5, 'malignant','benign')
+table(logit.pred, prep_biopsy$class)
+
+plot(result3)
